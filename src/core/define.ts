03302ca71be0a0524974e858171ec5667c291f82
feitingjun <449221252@qq.com>
2025-10-01 16:35:48 +0800

    修改注释
    Signed-off-by: feitingjun <449221252@qq.com>

--- src/core/define.ts
+++ src/core/define.ts
@@ -1,10 +1,18 @@
 import { FaeConfig, Plugin, Runtime } from './types'
 
-/**定义.faerc.ts用户配置 */
-export function defineFaeConfig(config: FaeConfig){ return config }
+/**定义用户配置 */
+export function defineFaeConfig(config: FaeConfig) {
+  return config
+}
 
 /**定义插件 */
-export function definePlugin<T extends Plugin|((...args:any[]) => Plugin)>(plugin:T){ return plugin }
+export function definePlugin<T extends Plugin | ((...args: any[]) => Plugin)>(
+  plugin: T
+) {
+  return plugin
+}
 
 /**定义运行时函数 */
-export function defineRuntime(fn:Runtime){ return fn }
+export function defineRuntime(fn: Runtime) {
+  return fn
+}
